5d7ef91f33694edb6fa22d535840b5f32401b5c0
ihongui <fihongui@st.ug.edu.gh>
2025-04-30 01:22:28 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const fadeElements = document.querySelectorAll(".fade-in");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    }, {
+      threshold: 0.1,
+    });
+  
+    fadeElements.forEach((el) => {
+      observer.observe(el);
+    });
+  });
+  
